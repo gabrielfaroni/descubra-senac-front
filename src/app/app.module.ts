1ead8f34d88aba3cf5fceda8bfd47b3e6fefd0c5
sala105b <sala105b@ceines.com.br>
2023-03-24 21:42:29 -0300

    commit meio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TesteComponent } from './components/teste/teste.component';
+import { CursosComponent } from './components/cursos/cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TesteComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
