d2ab207c6b8f0b04d32587bebb3f28e705fb9783
sala105b <sala105b@ceines.com.br>
2023-04-10 21:42:35 -0300

    switch break

--- src/app/components/teste/teste.component.ts
+++ src/app/components/teste/teste.component.ts
@@ -6,8 +6,110 @@ import { Component } from '@angular/core';
   styleUrls: ['./teste.component.css']
 })
 
-export class TesteComponent {
 
-  constructor() {}
+export class TesteComponent {
+  questao2: Boolean = false;
+  questao3: Boolean = false;
+  questao4: Boolean = false;
+  questao5: Boolean = false;
+  questao6: Boolean = false;
+  questao7: Boolean = false;
+  questao8: Boolean = false;
+  questao9: Boolean = false;
+  questao10: Boolean = false;
+  questao11: Boolean = false;
+
+
+
+  constructor() { }
+
+
+  acaoSim(questao: number): void {
+
+    switch (questao) {
+
+      case 1:
+        this.questao2 = true
+        break;
+
+      case 2:
+        this.questao4 = true
+        this.questao3 = false
+        break;
+      case 3:
+        // informatica basica
+        break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      default:
+        break;
+
+
+
+
+
+    }
+
+    this.questao2 = true;
+  }
+
+  acaoNao(questao: number): void {
+
+    switch (questao) {
+
+      case 1:
+        this.questao2 = true
+        break;
+
+      case 2:
+        this.questao3 = true
+        this.questao4 = false
+        break;
+      case 3:
+        this.questao5 = true
+        break;
+
+
+
+
+
+
+
+
+
+      default:
+        break;
+
+
+    }
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
